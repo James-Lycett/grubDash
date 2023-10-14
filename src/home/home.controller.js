5bfb06980f646809427a1ec902d10fe786180e29
James-Lycett <jrlycett@mail.com>
2023-10-14 11:09:49 -0700

    added url for readme on home page

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 // just a simple welcome message for the homepage
 function welcome(req, res, next) {
-    const message = "Welcome to the grubDash API"
-    res.send(message)
+    const welcomeMessage = "Welcome to the grubDash API, please refer to the readMe at https://github.com/James-Lycett/grubDash for instructions"
+    res.send(welcomeMessage)
 }
 
 // TODO: add request instructions for API users
